af2f6709bc7c04de48b520ce1ebf56ada107c180
himelpro <himeldebnathstudy7009@gmail.com>
2024-07-05 11:39:24 +0600

    pagalpanti

---
+++ src/main/java/com/himel/demo/newController.java
@@ -0,0 +1,35 @@
+package com.himel.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class newController {
+
+    @FXML
+    private TextField imgOf1stCNTextField;
+
+    @FXML
+    private TextField imgOf2ndCNTextField;
+
+    @FXML
+    private TextField realOf1stCNTextField;
+
+    @FXML
+    private TextField realOf2ndCNTextField;
+
+    @FXML
+    private Label resultLabel;
+
+    @FXML
+    void addComplexNumberButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void subtractComplexNumberButtonOnMouseClick(ActionEvent event) {
+
+    }
+
+}
